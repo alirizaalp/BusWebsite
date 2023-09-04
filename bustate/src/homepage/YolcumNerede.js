c01f7f3c3dbd6955b74c72f2f98a7c9476f07c06
alirizaalp <alralp@outlook.com>
2023-09-04 13:23:40 +0300

    location tracking app

--- bustate/src/homepage/YolcumNerede.js
+++ bustate/src/homepage/YolcumNerede.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-function YolcumNerede(){
-    return(
-        <div> 
-               BURAYA YOLCUNUN NEREDE OLDUĞUNU ARAÇA TANIMLI NAVİGASYON SİSTEMİ İLE GİRİLECEK.ANLIK VE KESİN BİLGİLER CEKİLECEK.BAĞLANTI KOPMASI DURUMUNDA İSE BİRAZDAN YOLCUNUZUN NEREDE OLDUĞUNUZU GÖRECEKSİNİZ YAZACAK.
+function YolcumNerede() {
+    const [location, setLocation] = useState(null);
+    const [connectionStatus, setConnectionStatus] = useState(true);
+
+
+    useEffect(() => {
+        const watchId = navigator.geolocation.watchPosition(
+            (position) => {
+                setLocation({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                });
+            },
+            (error) => {
+                console.error('Konum bilgisi alınamadı:', error);
+            }
+        );
+
+        const updateConnectionStatus = () => {
+            setConnectionStatus(navigator.onLine);
+        };
+
+        window.addEventListener('online', updateConnectionStatus);
+        window.addEventListener('offline', updateConnectionStatus);
+
+        return () => {
+
+            navigator.geolocation.clearWatch(watchId);
+            window.removeEventListener('online', updateConnectionStatus);
+            window.removeEventListener('offline', updateConnectionStatus);
+        };
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Location Tracking App</h1>
+            {location && (
+                <div>
+                    <p>Latitude: {location.latitude}</p>
+                    <p>Longitude: {location.longitude}</p>
+                </div>
+            )}
+            <p>Connection Status: {connectionStatus ? 'Online' : 'Offline'}</p>
         </div>
-    )
+    );
 }
 
 export default YolcumNerede;
